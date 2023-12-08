ab9807212f238ca432ebcb0cc715f893dcb24878
Mikołaj Skóra <you@example.com>
2023-12-08 17:51:08 +0100

    Initial Commit

---
+++ arrayList2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  arrayList2
+//
+//  Created by mikolaj on 08/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
